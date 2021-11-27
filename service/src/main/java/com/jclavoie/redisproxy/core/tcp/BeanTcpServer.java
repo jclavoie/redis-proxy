6463fa27d972bf2e24279514af5a3f1b2d304357
Jean-Christophe Lavoie <jeanchristophe.lavoie@logmein.com>
2021-11-27 15:38:28 -0500

    pign hte tcp server with a redis client in tests

--- service/src/main/java/com/jclavoie/redisproxy/core/tcp/BeanTcpServer.java
+++ service/src/main/java/com/jclavoie/redisproxy/core/tcp/BeanTcpServer.java
@@ -1,36 +1,57 @@
 package com.jclavoie.redisproxy.core.tcp;
 
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.StandardCharsets;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import reactor.netty.DisposableServer;
 import reactor.netty.tcp.TcpServer;
 
 @Component
-@RequiredArgsConstructor
+@Slf4j
 public class BeanTcpServer
 {
   @Value("${application.redis-proxy.tcp.hostname:localhost}")
   private final String hostname;
   @Value("${application.redis-proxy.tcp.port:6379}")
-  private final int port;
+  @Getter
+  private int port;
   private DisposableServer server;
+
+  public BeanTcpServer(final String hostname, final int port)
+  {
+    this.hostname = hostname;
+    this.port = port;
+  }
 
   @PostConstruct
   public void init()
   {
-    TcpServer
+    server = TcpServer
         .create()
         .host(hostname)
         .port(port)
         .handle((inbound, outbound) ->
         {
-          return outbound.send(inbound.receive().retain());
+          return outbound.send(
+              inbound.receive()
+                  .retain()
+                  .asString()
+                  .map(val -> Unpooled.copiedBuffer(val.getBytes(StandardCharsets.UTF_8))));
         }).bindNow();
+
+    if (port == 0)
+    {
+      port = server.port();
+    }
   }
 
   @PreDestroy
--- service/src/test/java/com/jclavoie/redisproxy/core/tcp/BeanTcpServerTest.java
+++ service/src/test/java/com/jclavoie/redisproxy/core/tcp/BeanTcpServerTest.java
@@ -7,18 +7,23 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 import reactor.netty.tcp.TcpClient;
+import reactor.test.StepVerifier;
 
 @SpringJUnitConfig
 @Slf4j
 public class BeanTcpServerTest
 {
-  
+
   @Test
   @SneakyThrows
   public void test()
@@ -42,4 +47,26 @@ public class BeanTcpServerTest
     latch.await(5, TimeUnit.SECONDS);
     assertEquals("Hello World!", response.get());
   }
+
+  @Test
+  @SneakyThrows
+  public void send_command()
+  {
+    final var server = new BeanTcpServer("localhost", 0);
+    server.init();
+    var redis = redisOperations(server.getPort());
+    var mono = redis.opsForValue().get("hello");
+    StepVerifier.create(mono)
+        .consumeNextWith(val -> log.info("{}", val))
+        .verifyComplete();
+  }
+
+  private ReactiveRedisOperations<String, String> redisOperations(final int port)
+  {
+    final var factory = new LettuceConnectionFactory("localhost", port);
+    factory.afterPropertiesSet();
+    return new ReactiveRedisTemplate(factory,
+        RedisSerializationContext.string());
+  }
+
 }
