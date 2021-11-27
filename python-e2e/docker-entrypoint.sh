51bb6772487dfe817ae643695982615172466b84
Jean-Christophe Lavoie <32459860+jclavoie-jive@users.noreply.github.com>
2021-11-26 23:16:12 -0500

    fix: Rework tests + add ttl + wrap config with env variables (#7)

--- python-e2e/docker-entrypoint.sh
+++ python-e2e/docker-entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-export SERVICE_URL="http://redis-proxy:8080"
-export REDIS_HOST="redis"
-export REDIS_PORT=6379
-export SERVICE_CACHE_SIZE=50
-export SERVICE_CACHE_TTL=20
-
-poetry run python main.py
+printenv
+poetry run python -u main.py
